adcac93ddc3c97cf9f71cac037369e01c2c27bc1
Наиль Юлтыев <yultyev@418team.com>
2026-09-01 20:19:58 +0300

    chore: update Nool Notch to 0.1.2

--- Casks/nool-notch.rb
+++ Casks/nool-notch.rb
@@ -1,6 +1,6 @@
 cask "nool-notch" do
-  version "0.1.1"
-  sha256 "32ba0fbf3fde224502cdd80b01d273abd6df7fb79f6ebc6eb00afad7c418500b"
+  version "0.1.2"
+  sha256 "fcacb66f02e439092c160d0d4a96206f3c17b74951dd060f7b9166dccaea548d"
 
   url "https://github.com/nail00749/nool_notch/releases/download/v#{version}/NoolNotch-v#{version}-arm64.zip"
   name "Nool Notch"
